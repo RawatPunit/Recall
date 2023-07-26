d61c348c6928f275bd3b39dac11888b1e17a5f6c
Punit Rawat <rawatpunit928@gmail.com>
2023-07-26 06:44:02 +0000

    API calls

---
+++ memories/src/api/index.js
@@ -0,0 +1,8 @@
+const custumFetch = () =>{
+    //segrigated the fetch() so that we dont have to write this again and again
+};
+
+const getPosts = (page, limit) => {
+    //limit ----- api call for limited number of pages
+    return customfetch();
+}
