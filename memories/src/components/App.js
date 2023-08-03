bb53dac20aa4d82f7b98b2533ab0436d79a38476
RawatPunit <rawatpunit928@gmail.com>
2023-08-03 21:41:49 +0530

    tweak

--- memories/src/components/App.js
+++ memories/src/components/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as  Route, Router, Routes } from "react-router-dom";
 
 import { getPosts } from '../api'
 import {Home , Login} from '../pages';
-import { Loader, Navbar,Comment } from './';
+import { Loader, Navbar } from './';
 
 const About =  () => {
   return <h1>About</h1>;
