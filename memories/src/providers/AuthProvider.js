4e0686f6e83ff69ab853e11e1bee9f346e235472
RawatPunit <rawatpunit928@gmail.com>
2023-08-04 12:24:40 +0530

    auth provider

---
+++ memories/src/providers/AuthProvider.js
@@ -0,0 +1,18 @@
+import { createContext } from "react";
+
+import { useProvideAuth } from "../hooks";
+
+const initialState = {
+    user : null,
+    login: () => {},
+    logout: () => {},
+    loading: true,
+}
+
+export const AuthContext = createContext(initialState)
+
+export const AuthProvider = ({ children }) => {
+    const auth = useProvideAuth();  //state managed by custom hook useprovideAuthso that value={auth} will be available to all the children
+
+    return <AuthContext.Provider value ={auth} >{children}</AuthContext.Provider> //this state is managed above useProvideAuth
+}
