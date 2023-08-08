3ea292fcdacc84b294116db0d5449581efb1f523
RawatPunit <rawatpunit928@gmail.com>
2023-08-08 20:46:08 +0530

    friend lists

--- memories/src/api/index.js
+++ memories/src/api/index.js
@@ -68,3 +68,9 @@ export const editProfile = async(userId,name, password, confirmPassword) => {
         body : { id:  userId,name ,  password, confirm_password: confirmPassword}
     });
 };
+
+export const fetchUserProfile = ( email, password ) => {            //this will be caleed in the user profile page
+    return customFetch(API_URLS.userInfo(userId),{
+        method : 'GET',
+    })
+}
