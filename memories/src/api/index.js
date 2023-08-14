e8822bff064571ecb076eec99bc60ee2d0f6f610
RawatPunit <rawatpunit928@gmail.com>
2023-08-14 17:32:03 +0530

    minor

--- memories/src/api/index.js
+++ memories/src/api/index.js
@@ -1,126 +1,132 @@
-import { API_URLS, LOCALSTORAGE_TOKEN_KEY } from '../utils'
+import { API_URLS, getFormBody, LOCALSTORAGE_TOKEN_KEY } from '../utils';
 
-const customFetch = async(url,{body, ...customConfig }) => {
-    //segrigated the fetch() so that we dont have to write this again and again
-    const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY)//while logging in Token generated and stored locally 
+const customFetch = async (url, { body, ...customConfig }) => {
+  //segrigated the fetch() so that we dont have to write this again and again
+  const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY); //while logging in Token generated and stored locally
 
-    const headers = {
-        'content-type': 'application/x-www-form-urlencoded',
+  const headers = {
+    'content-type': 'application/x-www-form-urlencoded',
+  };
+  if (token) {
+    //inside the headers pass the authorization key
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  const config = {
+    ...customConfig,
+    headers: {
+      ...headers,
+      ...customConfig.headers,
+    },
+  };
+
+  if (body) {
+    config.body = getFormBody(body); //imported from utils
+  }
+  try {
+    const response = await fetch(url, config); //config is like the methods and all
+    const data = await response.json();
+    if (data.success) {
+      return {
+        data: data.data,
+        success: true,
+      };
     }
-    if(token){      //inside the headers pass the authorization key 
-        headers.Authorization = `Bearer ${token}`;
-    } 
 
-    const config = {
-        ...customConfig,
-        headers:{
-            ...headers,
-            ...customConfig.headers,
-        },
+    throw new Error(data.message);
+  } catch (error) {
+    return {
+      message: error.message,
+      success: false,
     };
-
-    if(body){
-        config.body = getFormBody(body); //imported from utils
-    }
-    try{
-        const response = await fetch(url, config)   //config is like the methods and all
-        const data  =  await response.json()
-        if(data.success){
-            return{
-                data : data.data,
-                success : true
-            }
-        }
-
-        throw new Error(data.message);
-    }catch(error){
-        return{
-            message : error.message,
-            success : false,
-        }
-    }
+  }
 };
 
-export const getPosts = (page =1, limit = 5) => {
-    //limit ----- api call for limited number of pages
-    return customFetch(API_URLS.posts(page, limit),{
-        method : 'GET',
-    });
-}
-
-export const login = ( email, password ) => {
-    return customFetch(API_URLS.login(),{
-        method : 'POST',
-        body: {email, password },
-    })
-}
-
-export const register = async(name, email, password, confirmPassword) => { 
-    return customFetch(API_URLS.signup(),{
-        method : 'POST',
-        body : { name, email, password, confirm_password: confirmPassword}
-    });
+export const getPosts = (page = 1, limit = 5) => {
+  //limit ----- api call for limited number of pages
+  return customFetch(API_URLS.posts(page, limit), {
+    method: 'GET',
+  });
 };
 
-export const editProfile = async(userId,name, password, confirmPassword) => { 
-    return customFetch(API_URLS.editUser(),{
-        method : 'POST',
-        body : { id:  userId,name ,  password, confirm_password: confirmPassword}
-    });
+export const login = (email, password) => {
+  return customFetch(API_URLS.login(), {
+    method: 'POST',
+    body: { email, password },
+  });
 };
 
-export const fetchUserProfile = ( userId) => {            //this will be caleed in the user profile page
-    return customFetch(API_URLS.userInfo(userId),{
-        method : 'GET',
-    })
-}
+export const register = async (name, email, password, confirmPassword) => {
+  return customFetch(API_URLS.signup(), {
+    method: 'POST',
+    body: { name, email, password, confirm_password: confirmPassword },
+  });
+};
 
-export const fetchUserFriends = ( ) => {            //this will be caleed in the user profile page
-    return customFetch(API_URLS.friends,{
-        method : 'GET',
-    })
-}
+export const editProfile = async (userId, name, password, confirmPassword) => {
+  return customFetch(API_URLS.editUser(), {
+    method: 'POST',
+    body: { id: userId, name, password, confirm_password: confirmPassword },
+  });
+};
 
-export const addFriend = ( userId) => {            //this will be caleed in the user profile page
-    return customFetch(API_URLS.createFriendship(userId),{
-        method : 'POST',
-    })
-}
+export const fetchUserProfile = (userId) => {
+  //this will be caleed in the user profile page
+  return customFetch(API_URLS.userInfo(userId), {
+    method: 'GET',
+  });
+};
 
-export const removeFriend = ( userId) => {            //this will be caleed in the user profile page
-    return customFetch(API_URLS.removeFriend(userId),{
-        method : 'POST',
-    })
-}
+export const fetchUserFriends = () => {
+  //this will be caleed in the user profile page
+  return customFetch(API_URLS.friends, {
+    method: 'GET',
+  });
+};
 
-export const addPost = (content) => {            //this will be caleed in the user profile page
-    return customFetch(API_URLS.createPost(),{
-        method : 'POST',
-        body:{
-            content,
-        }
-    })
-}
+export const addFriend = (userId) => {
+  //this will be caleed in the user profile page
+  return customFetch(API_URLS.createFriendship(userId), {
+    method: 'POST',
+  });
+};
+
+export const removeFriend = (userId) => {
+  //this will be caleed in the user profile page
+  return customFetch(API_URLS.removeFriend(userId), {
+    method: 'POST',
+  });
+};
+
+export const addPost = (content) => {
+  //this will be caleed in the user profile page
+  return customFetch(API_URLS.createPost(), {
+    method: 'POST',
+    body: {
+      content,
+    },
+  });
+};
 
 export const createComment = async (content, postId) => {
-    return customFetch(API_URLS.comment(), {
-      method: 'POST',
-      body: {
-        post_id: postId,    //id so tht 1 cld know which post has the commt.
-        content,
-      },
-    });
-  };
+  return customFetch(API_URLS.comment(), {
+    method: 'POST',
+    body: {
+      post_id: postId, //id so tht 1 cld know which post has the commt.
+      content,
+    },
+  });
+};
 
-  export const toggleLike = (itemId, itemType) => {             //only one API for liking and disliking tooo...
-    return customFetch(API_URLS.toggleLike(itemId, itemType), {
-      method: 'POST',
-    });
-  };
-  
-  export const searchUsers = (searchText) => {
-    return customFetch(API_URLS.searchUsers(searchText), {
-      method: 'GET',
-    });
-  };
-  
+export const toggleLike = (itemId, itemType) => {
+  //only one API for liking and disliking tooo...
+  return customFetch(API_URLS.toggleLike(itemId, itemType), {
+    method: 'POST',
+  });
+};
+
+export const searchUsers = (searchText) => {
+  return customFetch(API_URLS.searchUsers(searchText), {
+    method: 'GET',
+  });
+};
